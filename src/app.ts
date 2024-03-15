1a04304404b5087518681a590df9ad109a9dc139
Bruno Felipe <brunofelipehp8@gmail.com>
2024-03-15 17:21:27 -0300

    finalizing user authentication routes

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,41 @@ app.use(express.json());
 
 const User = require("../models/User");
 
+app.get("/user/:id", checkToken, async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await User.findById(id, "-password");
+
+    if (!user) {
+      return res.status(401).json({ msg: "Usuário não encontrado" });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    return res.status(404).json({ msg: "Usuário não existe" });
+  }
+});
+
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: "Acesso negado" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+
+    jwt.verify(token, secret);
+
+    next();
+  } catch (error) {
+    return res.status(400).json({ msg: "Token invalido" });
+  }
+}
+
 app.get("/", (req, res) => {
   res.status(200).json({ msg: "Created successfully" });
 });
